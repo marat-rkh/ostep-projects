eb22e8f6c6ee74233aa58e63db378b58a181377f
Marat Khabibullin <marat.khabibullin@jetbrains.com>
2024-05-21 13:57:51 +0300

    wzip: finish

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -5,9 +5,8 @@ int main(int argc, char const* argv[]) {
         printf("wzip: file1 [file2 ...]\n");
         return 1;
     }
-    int lastChar = EOF;
-    int currentChar = EOF;
-    int count = -1;
+    int file_lastCharOfPrevFile = EOF;
+    int counter_count = 0;
     for (int i = 1; i < argc; i++) {
         const char* filename = argv[i];
         FILE* file = fopen(filename, "r");
@@ -15,29 +14,39 @@ int main(int argc, char const* argv[]) {
             printf("wzip: cannot open file '%s'\n", filename);
             return 1;
         }
-        if (lastChar == EOF) {
-            currentChar = fgetc(file);
-            count = 1;
-        } else {
-            currentChar = lastChar;
-        }
-        while (currentChar != EOF) {
-            int nextChar = fgetc(file);
-            if (nextChar == currentChar) {
-                ++count;
-            } else {
-                fwrite(&count, sizeof(int), 1, stdout);
-                fwrite(&currentChar, sizeof(char), 1, stdout);
-                count = 1;
+        int file_prevChar = EOF;
+        if (file_lastCharOfPrevFile == EOF) {
+            // This is the first file in the list.
+            int firstChar = fgetc(file);
+            if (firstChar == EOF) {
+                // Empty file, we skip it.
+                fclose(file);
+                continue;
             }
-            lastChar = currentChar;
-            currentChar = nextChar;
+            file_prevChar = firstChar;
+            // This is the first non empty file, so we start the counter.
+            counter_count = 1;
+        } else {
+            file_prevChar = file_lastCharOfPrevFile;
+        }
+        int file_currChar = fgetc(file);
+        while (file_currChar != EOF) {
+            if (file_currChar == file_prevChar) {
+                ++counter_count;
+            } else {
+                fwrite(&counter_count, sizeof(int), 1, stdout);
+                fwrite(&file_prevChar, sizeof(char), 1, stdout);
+                counter_count = 1;
+            }
+            file_prevChar = file_currChar;
+            file_currChar = fgetc(file);
         }
         fclose(file);
+        file_lastCharOfPrevFile = file_prevChar;
     }
-    if (count > 1) {
-        fwrite(&count, sizeof(int), 1, stdout);
-        fwrite(&lastChar, sizeof(char), 1, stdout);
+    if (counter_count > 0) {
+        fwrite(&counter_count, sizeof(int), 1, stdout);
+        fwrite(&file_lastCharOfPrevFile, sizeof(char), 1, stdout);
     }
     return 0;
 }
